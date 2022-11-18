38ba821ec55c63ef318e87a146d4014da07dae26
qardasha <107770203+qardasha@users.noreply.github.com>
2022-11-18 14:44:39 +1100

    Update function.js

--- function.js
+++ function.js
@@ -254,7 +254,7 @@ let scd_ID;
 
 function submitClicked(event) {
 	if (canSubmit) {
-		event.target.style.display = "none";
+		event.target.parentNode.style.display = "none";
 		username = event.target.elements.name.value;
 		username_text.innerHTML = username;
 		scd_ID = window.setInterval(startCountdown, 1000);
